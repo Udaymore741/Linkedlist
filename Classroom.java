a8e58cb188c618327243f4a5c95f730f28b0993d
Uday More <149674624+Udaymore741@users.noreply.github.com>
2024-08-16 00:46:33 +0530

    Add files via upload

--- Classroom.java
+++ Classroom.java
@@ -1,20 +1,46 @@
-import java.util.LinkedList;
-public class Classroom {
-    public static void main(String[] args) {
-        // create 
-        LinkedList<Integer> ll = new LinkedList<>();
-
-        // add
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addFirst(0);
-        //0->1->2
-        System.out.println(ll);
+class Solution {
+    static class Node {
     
-
-        // remove 
-        ll.removeLast();
-        ll.removeFirst();
-        System.out.println(ll);
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+    public Node getIntersectionNode(Node head1, Node head2){
+        while (head2 != null) {
+            Node temp = head1;
+            while (temp != null) {
+                if (temp == head2) {
+                    return head2;
+                }
+                temp = temp.next;
+            }
+            head2 = head2.next;
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        Solution list = new Solution();
+        Node head1, head2;
+        head1 = new Node(10);
+        head2 = new Node(3);
+        Node newNode = new Node(6);
+        head2.next = newNode;
+        newNode = new Node(9);
+        head2.next.next = newNode;
+        newNode = new Node(15);
+        head1.next = newNode;
+        head2.next.next.next = newNode;
+        newNode = new Node(30);
+        head1.next.next = newNode;
+        head1.next.next.next = null;
+        Node intersectionPoint= list.getIntersectionNode(head1, head2);
+        if (intersectionPoint == null) {
+            System.out.print(" No Intersection Point \n");
+        }else {
+            System.out.print("Intersection Point: "+ intersectionPoint.data);
+        }
     }
 }
